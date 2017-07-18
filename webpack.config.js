68139a280d857afbcd8d52fdc4a8bd764150344b
Trinh Thanh Phuong <iphuongtt@gmail.com>
2017-07-19 06:34:33 +0700

    minifying and optimizing images with image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,34 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(png|svg|jpg|gif)$/,
+                test: /\.(png|svg|jpg|gif)$/i,
                 use: [
-                    'file-loader'
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        query: {
+                            progressive: true,
+                            optimizationLevel: 7,
+                            interlaced: false,
+                            mozjpeg: {
+                                quality: 65
+                            },
+                            pngquant:{
+                                quality: "65-90",
+                                speed: 4
+                            },
+                            svgo:{
+                                plugins: [
+                                    {
+                                        removeViewBox: false
+                                    },
+                                    {
+                                        removeEmptyAttrs: false
+                                    }
+                                ]
+                            }
+                        }
+                    }
                 ]
             }
         ]
