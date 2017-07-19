5cd5984ed730bcd6b0cb1e5f3148ed69dfc2d64b
Trinh Thanh Phuong <iphuongtt@gmail.com>
2017-07-19 11:58:54 +0700

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import _ from 'lodash';
+import printMe from './print';
 function component() {
     var element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    var btn = document.createElement('button');
+    btn.innerHTML = 'Click me and check the console';
+    btn.onclick = printMe;
+    element.appendChild(btn);
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
-var path = require('path');
-
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ]
 };
