215d6e50472e8fff88a3ed0177de4959a1b0276f
Trinh Thanh Phuong <iphuongtt@gmail.com>
2017-07-19 13:21:45 +0700

    Using webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
         print: './src/print.js'
     },
     devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
